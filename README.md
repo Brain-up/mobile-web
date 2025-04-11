b081e6da6665ec9b00bcd27e858c60445b91f1d3
pedro gongora <pedro.gongora@sephora.com>
2025-04-11 16:44:10 -0600

    added readme

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
+
 # mobile-web
-for mobile app
 
+Simple mobile app consisting of a web view container of https://www.brainup.site
 
+To run the app simply sync and run de developDebug build variant of the app.
+
+To deploy to the app store get a **keystore.properties** file with the required configuration along with the **keystore** to generate a signed release file.
+
+Sample **keystore.properties** file:
+
+    storeFile=[/path/to/keystore.jks]
+    storePassword=[password]  
+    keyAlias=[key alias]  
+    keyPassword=[password]  
+
+to build a bundle -> **./gradlew bundleProdRelease**
+apk -> **./gradlew assembleProdRelease**
+
+Contact:
 https://t.me/+c5-JlYtQNcNmZjUy dev chat
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,15 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+}
+
+val keystorePropertiesFile = rootProject.file("keystore.properties")
+val keystoreProperties = Properties()
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 }
 
 android {
@@ -18,10 +27,20 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = true
             isDebuggable = false
+            signingConfig = signingConfigs.getByName("release")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
